8ad87d33c90641fd3a0847c34ecfde369184f6c8
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-02-27 15:12:42 +0530

    new commit

--- src/manageApi/context/AuthContext.jsx
+++ src/manageApi/context/AuthContext.jsx
@@ -13,8 +13,8 @@ import {
 export const AuthContext = createContext();
 
 // const API_BASE = 'https://kotiboxglobaltech.online/api';
-const API_BASE = 'http://localhost:5000/api';
-// const API_BASE = 'https://xoto.ae/api';
+// const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://xoto.ae/api';
 
 
 export const AuthProvider = ({ children }) => {
