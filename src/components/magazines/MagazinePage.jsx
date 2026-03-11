754947554d00542dba962fddfdb6e9c8c7dc131a
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-03-11 10:13:45 +0530

    changes in observation table "

--- src/components/magazines/MagazinePage.jsx
+++ src/components/magazines/MagazinePage.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import wave1 from "../../assets/img/wave/wave1.png";
-// import round from "../../assets/img/round231.mp4";
+// import round from "../../assets/img/round1.mp4";
 import rating from "../../assets/icons/Homeicons/rating.png";
 import partner from "../../assets/icons/Homeicons/partners.png";
 import vector from "../../assets/icons/Homeicons/Vector.png";
