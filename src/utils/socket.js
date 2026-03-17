b277eb5cf9daf80b4aad46cdeba52bf5e47bb94b
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-03-17 19:02:28 +0530

    sdfsd

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export const getSocket = () => {
   if (!socket || socket.disconnected) {
-    socket = io("http://localhost:5000", {
+    socket = io("https://xoto.ae", {
       transports: ["websocket"],
       reconnection: true,
     });
