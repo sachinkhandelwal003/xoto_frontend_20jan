849a8e747c0f1495c287f617947b4e205faa2b4b
sachinkhandelwal003 <skhandelwal44@gmail.com>
2026-03-23 17:29:44 +0530

    xobia animationna upodate

--- src/manageApi/utils/custom.apiservice.js
+++ src/manageApi/utils/custom.apiservice.js
@@ -2,9 +2,9 @@
 import axios from 'axios'; 
 import { showToast } from './toast';
 
-const API_BASE_URL = 'http://localhost:5000/api/';
+// const API_BASE_URL = 'http://localhost:5000/api/';
 // const API_BASE_URL = 'https://kotiboxglobaltech.online/api/';
-// const API_BASE_URL = 'https://xoto.ae/api/';
+const API_BASE_URL = 'https://xoto.ae/api/';
 
             
 // Create axios instance dfdsgsdg
